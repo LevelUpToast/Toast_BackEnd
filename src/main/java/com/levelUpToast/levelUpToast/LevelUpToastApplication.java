84e23ee51bf44ebc94bc1d82a494738d3a4fb847
JiYongKim <colorful8315@gmail.com>
2022-07-27 18:56:12 +0900

    Create project frame

---
+++ src/main/java/com/levelUpToast/levelUpToast/LevelUpToastApplication.java
@@ -0,0 +1,13 @@
+package com.levelUpToast.levelUpToast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelUpToastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelUpToastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/levelUpToast/levelUpToast/LevelUpToastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelUpToast.levelUpToast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpToastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
