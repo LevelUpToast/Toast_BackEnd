1d490a07d524083fbf619eaf80514d1af029fa8d
JiYongKim <colorful8315@gmail.com>
2022-07-28 19:48:28 +0900

    feat (LoginService) : Add LoginService Interface & SimpleLoginService class

---
+++ src/main/java/com/levelUpToast/levelUpToast/domain/login/loginServiceImpl/SimpleLoginService.java
@@ -0,0 +1,7 @@
+package com.levelUpToast.levelUpToast.domain.login.loginServiceImpl;
+
+import com.levelUpToast.levelUpToast.domain.login.loginServiceInterface.LoginService;
+
+public class SimpleLoginService implements LoginService {
+
+}
